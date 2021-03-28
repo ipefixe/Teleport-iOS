2de31f5506ba6577bfbad3f6fbd1b0fec64681d3
Ipefixe <ipefixe.fr@gmail.com>
2021-03-28 23:01:54 +0200

    Initial Commit

---
+++ Teleport/TeleportApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeleportApp.swift
+//  Teleport
+//
+//  Created by Kevin Boulala on 28/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TeleportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
