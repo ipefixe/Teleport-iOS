6ec89dcaf103701b6adc34301ac77ad3e29857ca
Ipefixe <ipefixe.fr@gmail.com>
2021-03-28 23:10:59 +0200

    Init repo

---
+++ Teleport/Modules/CitySearch/CitySearchView.swift
@@ -0,0 +1,16 @@
+//
+//  CitySearchView.swift
+//  Teleport
+//
+//  Created by Kevin Boulala on 28/03/2021.
+//
+
+import SwiftUI
+
+struct CitySearchView: View {
+    
+    var body: some View {
+        Text("Search View")
+        .navigationBarTitle("Teleport")
+    }
+}
